3871ff725d712383be10d725c190285d4d770de7
Remon <remon.onnorokom@gmail.com>
2025-04-08 12:15:40 +0600

    no changes

--- Learning ASP.NET Core/Learn.AspNetCore/obj/Debug/net8.0/Learn.AspNetCore.AssemblyInfo.cs
+++ Learning ASP.NET Core/Learn.AspNetCore/obj/Debug/net8.0/Learn.AspNetCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learn.AspNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d7eaa7de54604b721a51e191407327b434a579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75868939215f84bab3d8e38eecd8ace51d0a506")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learn.AspNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learn.AspNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
